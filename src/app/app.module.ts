33fc32f4df4d5d281e63284885a2abf24689fa4b
bsralkan <bsr.alkan@gmail.com>
2020-10-02 14:58:56 +0300

    Lesson 2: Generating New Component Using CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
