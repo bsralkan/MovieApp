b195ef559f441bc6b6074b95ebc08d30009cdac3
bsralkan <bsr.alkan@gmail.com>
2020-10-05 09:22:57 +0300

    Logging card added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { LoggingComponent } from './logging/logging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     MovieComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    LoggingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import { Observable, observable, of} from 'rxjs'
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   getMovies(): Observable<Movie[]> {
+    this.loggingService.add('MovieService: listing movies');
     return of(Movies);
   }
 } 
